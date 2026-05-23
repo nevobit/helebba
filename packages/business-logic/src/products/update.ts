1f11516adcaa5c0d5419eb722c1b3c97af574486
nestor <nevobit.global@gmail.com>
2026-05-23 14:48:36 -0500

    chore: add soft delete and get by id to products logic

--- packages/business-logic/src/products/update.ts
+++ packages/business-logic/src/products/update.ts
@@ -1,12 +1,15 @@
 import { Collection, getModel } from '@hlb/constant-definitions';
-import { LifecycleStatus, type Product, type ProductId, ProductSchemaMongo } from '@hlb/contracts';
+import { type Product, type ProductId, ProductSchemaMongo } from '@hlb/contracts';
 
-export const softDeleteProduct = async (productId: ProductId) => {
+export const updateProduct = async (productId: ProductId, data: Partial<Product>) => {
   const model = getModel<Product>(Collection.PRODUCTS, ProductSchemaMongo);
-  const result = await model.updateOne(
-    { _id: productId },
-    { $set: { lifecycleStatus: LifecycleStatus.DELETED } },
-  );
+
+  const dataToUpdate = {
+    ...data,
+    updatedAt: new Date().toISOString(),
+  };
+
+  const result = await model.updateOne({ _id: productId }, { $set: dataToUpdate });
 
   if (!result.acknowledged && result.matchedCount < 1) throw new Error('Could not update product');
 
